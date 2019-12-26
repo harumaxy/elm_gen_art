08cfa2cac30ba3f3eae6c2e146001d0261f895f5
Masaharu Hosomichi <masaharuhosomichi@MasaharunoiMac.local>
2019-12-26 11:19:02 +0900

    波紋のアニメーションを実装

---
+++ src/Lib/Ripple.elm
@@ -0,0 +1,73 @@
+module Lib.Ripple exposing (..)
+
+import Browser.Events exposing (..)
+import Canvas exposing (..)
+import Canvas.Settings exposing (..)
+import Color
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Events.Extra.Mouse as Mouse
+import List exposing (..)
+
+
+
+-- Model
+
+
+type Model
+    = List Ripple
+
+
+type alias Ripple =
+    { pos : ( Float, Float ), r : Float, maxr : Float }
+
+
+
+-- Update
+
+
+type Msg
+    = ClickedAt ( Float, Float )
+
+
+update : List Ripple -> Float -> List Ripple
+update model dt =
+    let
+        speed =
+            100
+    in
+    model
+        |> List.map (\item -> { item | r = item.r + speed * dt / 1000 })
+        |> List.filter (\item -> item.r < item.maxr)
+
+
+
+-- View
+
+
+view model =
+    let
+        w =
+            1000
+
+        h =
+            1000
+    in
+    Canvas.toHtml ( w, h )
+        [ Mouse.onDown (\event -> Debug.log "clicked!" (ClickedAt event.offsetPos)) ]
+        ([ background w h
+         , shapes [ fill Color.red ] [ circle ( 0, 0 ) 100 ]
+         ]
+            ++ List.map rippleItem model
+        )
+
+
+background w h =
+    shapes [ fill (Color.rgb255 51 51 51), stroke Color.black ]
+        [ rect ( 0, 0 ) w h ]
+
+
+rippleItem : Ripple -> Renderable
+rippleItem item =
+    shapes [ fill (Color.rgba 1 1 1 (1 - item.r / item.maxr)) ] [ circle item.pos item.r ]
--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,8 @@ import Browser
 import Browser.Events
 import Browser.Navigation as Nav
 import Html exposing (..)
+import Lib.Ripple as Ripple
+import Random exposing (..)
 import Url
 
 
@@ -22,24 +24,42 @@ main =
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
+    , time : Float
     , dt : Float
+
+    -- Ripple
+    , ripples : List Ripple.Ripple
     }
 
 
-init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init flags url key =
-    ( Model key url 0, Cmd.none )
+
+-- Update
 
 
 type Msg
     = UrlRequested Browser.UrlRequest
     | UrlChanged Url.Url
     | UpdateFrame Float
+    | RMsg Ripple.Msg
+    | CreateRipple ( Float, Float ) Float
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( Model
+        key
+        url
+        0
+        0
+        []
+    , Cmd.none
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        -- Main
         UrlRequested urlRequest ->
             case urlRequest of
                 Browser.Internal url ->
@@ -54,7 +74,23 @@ update msg model =
             )
 
         UpdateFrame dt ->
-            ( { model | dt = dt }, Cmd.none )
+            ( { model | dt = dt, ripples = Ripple.update model.ripples dt }, Cmd.none )
+
+        -- Ripple
+        RMsg rmsg ->
+            let
+                maxRound =
+                    200
+
+                minRound =
+                    50
+            in
+            case rmsg of
+                Ripple.ClickedAt pos ->
+                    ( model, Random.generate (CreateRipple pos) (Random.float minRound maxRound) )
+
+        CreateRipple pos r ->
+            ( { model | ripples = Ripple.Ripple pos 0 r :: model.ripples }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -69,5 +105,6 @@ view model =
         [ div [] [ text "New Application" ]
         , h1 [] [ text (Url.toString model.url) ]
         , h1 [] [ text ("update per millisec" ++ String.fromFloat model.dt) ]
+        , Html.map RMsg (Ripple.view model.ripples)
         ]
     }
